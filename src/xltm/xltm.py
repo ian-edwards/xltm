7d832f3345e2b330ff17749eb47ffed6abb9ac75
Ian Edwards <ian.philip.edwards@gmail.com>
2024-12-08 10:18:25 +0000

    further sheet refactoring

--- src/xltm/xltm.py
+++ src/xltm/xltm.py
@@ -10,7 +10,7 @@ def read_images(xlfile: ZipFile):
     return {p[14:] : xlfile.read(p) for p in xlfile.namelist() if p.startswith('xl/media/image')}
 
 def read_sheets(xlfile: ZipFile):
-    return {e.get('name') + '.csv' : cells_matrix(read_cells(f'xl/worksheets/sheet{e.get('sheetId')}.xml', xlfile)) for e in xml_elements('sheet', read_file('xl/workbook.xml', xlfile))}
+    return {e.get('name') + '.csv' : cells_matrix(read_cells(f'xl/worksheets/sheet{e.get('sheetId')}.xml', xlfile)) for e in read_xml('xl/workbook.xml', xlfile).findall(f'.//{{*}}sheet')}
 
 def write_images(outdir: str, images: dict[str, bytes]):
     for (name, data) in images.items():
@@ -24,7 +24,7 @@ def cells_matrix(cells: dict[(int, int), int]) -> list[list[str]]:
     return [[cells.get((r, c)) for c in range(columns_count(cells))] for r in range(rows_count(cells))]
 
 def read_cells(sheetpath: str, xlfile: ZipFile):
-    return {(cell_row(a := c.get('r')), cell_column(a)) : int(c.get('vm')) for c in xml_elements('c', read_file(sheetpath, xlfile))}
+    return {(cell_row(a := c.get('r')), cell_column(a)) : int(c.get('vm')) for c in read_xml(sheetpath, xlfile).findall(f'.//{{*}}c')}
 
 def serialize_csv(cells: list[int]):
     return '\n'.join([','.join([str(c) if c is not None else '' for c in r]) for r in cells])
@@ -44,11 +44,8 @@ def rows_count(cells: dict[(int, int), int]):
 def columns_count(cells: dict[(int, int), int]):
     return max([k[1] for k in cells.keys()]) + 1
 
-def read_file(path: str, xl: ZipFile):
+def read_xml(path: str, xl: ZipFile):
     return ElementTree.parse(BytesIO(xl.read(path)))
-
-def xml_elements(path: str, tree: ElementTree):
-    return tree.findall(f'.//{{*}}{path}')
 
 if __name__ == "__main__":
     if (largs := len(args := sys.argv)) > 1:
